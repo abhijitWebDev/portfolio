ef08d7f2361ddf1c12a73405ddb49cc40fbd4431
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2024-06-30 23:13:04 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <h1 className="text-red-500"></h1>
       <div className="card">
         <h1 className="text-blue-600">Welcome to Abhijit's World of Web Development</h1>
-        <h2>Projects comming soon, Stay tuned</h2>
+        <h2>Projects coming soon, Stay tuned</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
