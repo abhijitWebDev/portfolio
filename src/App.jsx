a51708cccaa4008d93188ab3cf7a511448bc70b3
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2024-06-30 22:54:41 +0530

    changed the image

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <h1 className="text-red-500"></h1>
       <div className="card">
-        <h1 className="text-blue-600">Welcome to Abhijit world of web development</h1>
+        <h1 className="text-blue-600">Welcome to Abhijit's World of Web Development</h1>
         <h2>Projects comming soon, Stay tuned</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
